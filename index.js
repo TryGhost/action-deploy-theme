808a1fd70ffb9019d1725514619a4c622ef4dbc1
Hannah Wolfe <erisds@gmail.com>
2019-10-11 16:10:37 +0100

    ✨ Working zip+deploy action
    - First pass at code to create a working action
    - Was tested locally on a theme repo first

---
+++ index.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const core = require('@actions/core');
+const exec = require('@actions/exec');
+const GhostAdminApi = require('@tryghost/admin-api');
+
+(async function main() {
+    const url = core.getInput('api-url');
+    const api = new GhostAdminApi({
+        url,
+        key: core.getInput('api-key'),
+        version: 'canary'
+    });
+
+    const basePath = process.env.GITHUB_WORKSPACE;
+    const pkgPath = path.join(process.env.GITHUB_WORKSPACE, 'package.json');
+    const themeName = require(pkgPath).name;
+    const themeZip = `${themeName}.zip`;
+    const zipPath = path.join(basePath, themeZip);
+    const exclude = core.getInput('exclude') || '';
+
+    try {
+        // Create a zip
+        await exec.exec(`zip -r ${themeZip} . -x *.git* *.zip yarn* npm* *routes.yaml *redirects.yaml *redirects.json ${exclude}`, [], {cwd: basePath});
+
+        // Deploy it to the configured site
+        await api.themes.upload({file: zipPath});
+        console.log(`${themeZip} successfully uploaded.`);
+    } catch (err) {
+        console.error(err);
+        process.exit(1);
+    }
+}());
