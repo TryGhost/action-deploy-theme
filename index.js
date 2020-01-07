a7c05672b043d5543cdfb4442c62a522efeb3f2f
Vikas Potluri <vikaspotluri123.github@gmail.com>
2020-01-07 17:04:30 -0600

    :sparkles: added support for uploading pre-generated zip file
    closes #1
    
    - Add a new `file` option which uploads the specified file instead of generating one

--- index.js
+++ index.js
@@ -14,17 +14,25 @@ const GhostAdminApi = require('@tryghost/admin-api');
 
         const basePath = process.env.GITHUB_WORKSPACE;
         const pkgPath = path.join(process.env.GITHUB_WORKSPACE, 'package.json');
-        const themeName = core.getInput('theme-name') || require(pkgPath).name;
-        const themeZip = `${themeName}.zip`;
-        const zipPath = path.join(basePath, themeZip);
-        const exclude = core.getInput('exclude') || '';
 
-        // Create a zip
-        await exec.exec(`zip -r ${themeZip} . -x *.git* *.zip yarn* npm* *routes.yaml *redirects.yaml *redirects.json ${exclude}`, [], {cwd: basePath});
+        let zipPath = core.getInput('file');
+
+        // Zip file was not provided - zip everything up!
+        if (!zipPath) {
+            const themeName = core.getInput('theme-name') || require(pkgPath).name;
+            const themeZip = `${themeName}.zip`;
+            const exclude = core.getInput('exclude') || '';
+            zipPath = themeZip;
+
+            // Create a zip
+            await exec.exec(`zip -r ${themeZip} . -x *.git* *.zip yarn* npm* *routes.yaml *redirects.yaml *redirects.json ${exclude}`, [], {cwd: basePath});
+        }
+
+        zipPath = path.join(basePath, zipPath);
 
         // Deploy it to the configured site
         await api.themes.upload({file: zipPath});
-        console.log(`${themeZip} successfully uploaded.`);
+        console.log(`${zipPath} successfully uploaded.`);
     } catch (err) {
         console.error(err);
         process.exit(1);
